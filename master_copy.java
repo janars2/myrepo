b0dd767375ef9105667e2a5dfde459c5ff4eb4da
Janardhan, Shilpa <Shilpa.Janardhan@emc.com>
2016-04-18 21:02:41 +0530

    new file

---
+++ master_copy.java
@@ -0,0 +1,33 @@
+package org.somtest.poc;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+     
+    {
+	//changes in the branch
+	//first commit
+	//second commit
+	//test commit
+    	String filenamePrefix = "esa0000";
+    	for(int i=22;i<164;i++){
+    		File file = new File("C:\\work\\10.6\\reporting_10.6\\ASOC-1529\\poc\\src\\main\\java\\org\\somtest\\poc\\"+filenamePrefix+i);
+            if (!file.exists()) {
+                try {
+					file.createNewFile();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+    	}
+    	
+    }
+}
